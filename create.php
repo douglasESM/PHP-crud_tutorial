cd2e3e0283ba4218133f588bfbe7bfbbae93ecd0
Douglas Morais <douglaserik.tec@gmail.com>
2019-03-13 18:56:53 -0400

    Commit dos arquivos locais

---
+++ create.php
@@ -0,0 +1,109 @@
+<?php
+	require 'database.php';
+
+	if ( !empty($_POST)){
+		//KEEP TRACK VALIDATION ERRORS
+		//ACOMPANHA OS ERROS DE VALIDAÇÃO
+		$nameError = null;
+		$emailError = null;
+		$mobileError = null;
+
+		//KEEP TRACK POST VALUES
+		//ACOMPANHAR VALORES DO POST
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$mobile = $_POST['mobile'];
+
+		//VALIDATE INPUT
+		//VALIDAR ENTRADA
+		$valid = true;
+		if (empty($name)){
+			$nameError = 'Please enter Name';
+			$valid = false;
+		}
+
+		if (empty($name)){
+			$emailError = 'Please enter Email Address';
+			$valid = false;
+		}else if ( !filter_var($email, FILTER_VALIDATE_EMAIL)){
+			$emailError = 'Please enter a valid Email Address';
+			$valid = false;
+		}
+
+		if (empty($mobile)){
+			$mobileError = 'Please enter Mobile Number';
+			$valid = false;
+		}
+
+		//INSERT DATA
+		//INSERINDO DADOS
+		if($valid) {
+			$pdo = Database::connect();
+			$pdo->serAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "INSERT INTO customers (name, email, mobile) VALUES (?; ?; ?)";
+			$q-> $pdo->prepare($sql);
+			$q->execute(array($name, $email, $mobile));
+			Database::disconnect();
+			header("Location: index.php");
+		}
+	}
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+ 
+<body>
+    <div class="container">
+     
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Create a Customer</h3>
+                    </div>
+             
+                    <form class="form-horizontal" action="create.php" method="post">
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+                        <label class="control-label">Email Address</label>
+                        <div class="controls">
+                            <input name="email" type="text" placeholder="Email Address" value="<?php echo !empty($email)?$email:'';?>">
+                            <?php if (!empty($emailError)): ?>
+                                <span class="help-inline"><?php echo $emailError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($mobileError)?'error':'';?>">
+                        <label class="control-label">Mobile Number</label>
+                        <div class="controls">
+                            <input name="mobile" type="text"  placeholder="Mobile Number" value="<?php echo !empty($mobile)?$mobile:'';?>">
+                            <?php if (!empty($mobileError)): ?>
+                                <span class="help-inline"><?php echo $mobileError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Create</button>
+                          <a class="btn" href="index.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
+        
---
+++ customers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE customers(
+    id int not null auto_increment primary key,
+    name varchar(100) not null,
+    EMAIL varchar(100) not null,
+    mobile varchar(100) not null
+)engine = INNOBD,u~[o.v´9vp-8co-cx0y]
